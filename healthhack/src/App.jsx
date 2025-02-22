7037b8150020896ffb2d9e4de6277925bfb2fb03
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2025-02-22 23:30:06 +0800

    first commit

---
+++ healthhack/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Demo from './HandTrackingOverlay';
+
+function App() {
+  return (
+    <>
+      <h1>Hand Tracking with MediaPipe</h1>
+      <Demo />
+    </>
+  );
+}
+
+export default App;
